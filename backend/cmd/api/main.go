8d2f646a107d056f254a331bd82d78edadb64884
Snow-00 <wariorwilly43@gmail.com>
2024-01-24 12:53:31 +0700

    get data from flag

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,16 +1,21 @@
 package main
 
+import "flag"
+
 const PORT = "8080"
 
 type Application struct {
 	Domain string
+	DSN    string // data source name
 }
 
 func main() {
 	// set app config
 	var app Application
 
-	// read from command line
+	// read from command line, think still better .env
+	flag.StringVar(&app.DSN, "dsn", "HOST=localhost PORT=5432 USER=postgres PASSWORD=postgres DBNAME=movies SSLMODE=disable TIMEZONE=UTC CONNECT_TIMEOUT=5", "Postgres connection string") // timeout 5 secs
+	flag.Parse()
 
 	// connect to db
 	app.Domain = "example.com"
