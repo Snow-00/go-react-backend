3019d0f48678f859cb0ccceb2e501256d5347a5f
Snow-00 <wariorwilly43@gmail.com>
2024-01-27 23:50:46 +0700

    generate token

--- backend/cmd/api/auth.go
+++ backend/cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -18,16 +19,64 @@ type Auth struct {
 }
 
 type JWTUser struct {
-	ID int `json:"id"`
+	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
+	LastName  string `json:"last_name"`
 }
 
 type TokenPairs struct {
-	Token string `json:"access_token"`
+	Token        string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
 
-type Claims struct {
-	jwt.RegisteredClaims
+// type Claims struct {
+// 	jwt.RegisteredClaims
+// }
+
+func (j *Auth) GenerateTokenPair(user *JWTUser) (TokenPairs, error) {
+	// create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID) // subject
+	claims["aud"] = j.Audience          // audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix() //issue at
+	claims["typ"] = "JWT"
+
+	// set expiry for jwt
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// create signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// create refresh token and set claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// set expiry for refresh token
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	// create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	//create TokenPairs and populate with signed tokens
+	tokenPairs := TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// return TokenPairs
+	return tokenPairs, nil
 }
