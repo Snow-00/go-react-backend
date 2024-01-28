f0cc57d2be6e2488fa3a14c84cce0536918a4f45
Snow-00 <wariorwilly43@gmail.com>
2024-01-28 20:11:42 +0700

    set values for jwt token

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
 	"github.com/Snow-00/go-react-movies-backend/internal/repository"
 	"github.com/Snow-00/go-react-movies-backend/internal/repository/dbrepo"
@@ -11,9 +12,14 @@ import (
 const PORT = "8080"
 
 type Application struct {
-	Domain string
-	DSN    string // data source name
-	DB repository.DatabaseRepo
+	Domain       string
+	DSN          string // data source name
+	DB           repository.DatabaseRepo
+	Auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
@@ -22,6 +28,11 @@ func main() {
 
 	// read from command line, think still better .env
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string") // timeout 5 secs
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "Signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "Signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "Signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "Cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "Domain")
 	flag.Parse()
 
 	// connect to db
@@ -32,8 +43,17 @@ func main() {
 
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close()
-	
-	app.Domain = "example.com"
+
+	app.Auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token", // this is related to naming convention
+		CookieDomain:  app.CookieDomain,
+	}
 
 	// start web server
 	app.Routes().Run(":" + PORT)
