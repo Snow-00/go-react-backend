5a01fa35450961bcdd77277687abacd666afc4d4
Snow-00 <wariorwilly43@gmail.com>
2024-03-08 20:55:48 +0700

    change cors middleware

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -9,8 +9,8 @@ import (
 func (app *Application) EnableCORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// c.Header("Access-Control-Allow-Origin", "https://supreme-halibut-v664446pgxqxhwxvr-3000.app.github.dev")
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-		// c.Header("Access-Control-Allow-Origin", "https://learn-code.ca")
+		// c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Origin", "https://go-react-frontend.vercel.app")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, X-CSRF-Token, Authorization")
